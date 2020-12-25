76122d0c86629bf716bbab09cb297b3869fcc59e
liuxuanqi <1369411829@qq.com>
2020-12-25 17:36:07 +0800

    创建项目，构建crud子模块

---
+++ webflux-demo-crud/src/main/java/com/webflux/crud/demo/service/TestService.java
@@ -0,0 +1,13 @@
+package com.webflux.crud.demo.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.webflux.crud.demo.entity.TestDO;
+
+/**
+ * @Description 服务类接口
+ * @author lxq
+ * @since 2020-09-20
+ */
+public interface TestService extends IService<TestDO> {
+
+}
